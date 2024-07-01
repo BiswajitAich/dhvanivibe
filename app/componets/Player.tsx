b89047c5a7c5ee2223066f38f8b5bb6c9a528eed
Biswajit Aich <0000biswajitaich@gmail.com>
2024-07-01 11:56:31 +0530

    player screen volume bar(2nd day)

--- app/componets/Player.tsx
+++ app/componets/Player.tsx
@@ -4,11 +4,14 @@ import style from "@/app/css/player.module.css";
 import Image from "next/image";
 import { SongContext } from "./context/SongContextProvider";
 import noImage from '@/public/no-image.webp'
+import volMute from '@/public/volume-icon-mute.png'
+import vol from '@/public/volume-icon.png'
 // import PlayerScreen from "./playerScreen";
 
 const Player: React.FC<any> = () => {
   const [audioSrc, setAudioSrc] = useState<string[]>([]);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [volumeProgress, setVolumeProgress] = useState<number>(0.50);
   const [displayPlayerScreen, setDisplayPlayerScreen] = useState<boolean>(false);
   const [audioReady, setAudioReady] = useState<boolean>(false);
   const [currentTimeTrack, setCurrentTimeTrack] = useState<string>("(00:00)");
@@ -102,6 +105,16 @@ const Player: React.FC<any> = () => {
     setDisplayPlayerScreen(!displayPlayerScreen)
   }
 
+  const handleDataFromChild = (data: number) => {
+    setVolumeProgress(data);
+  };
+
+  useEffect(()=>{
+    if(songRef.current){
+      songRef.current.volume = Number(volumeProgress);
+    }
+  },[volumeProgress])
+
   return (<>
     {
       displayPlayerScreen ?
@@ -109,7 +122,7 @@ const Player: React.FC<any> = () => {
           <div className={style.playerScreenHeader}>
             <button type="button" onClick={handleDisplayPlayerScreen}>&#10148;</button>
           </div>
-          <PlayerScreen data={currentSongData} />
+          <PlayerScreen data={currentSongData} volumeProgress={volumeProgress} sendDataToParent={handleDataFromChild} />
         </div>
         : null
     }
@@ -130,14 +143,14 @@ const Player: React.FC<any> = () => {
           {audioSrc[1] ? <source key={1} src={audioSrc[1]} type="audio/mpeg" /> : null}
         </audio>
       )}
-{/* 
+      {/* 
       <button
         onClick={handleDisplayPlayerScreen}
         className={style.displayPlayerScreen}>
         &#10148;
       </button> */}
 
-      {currentSongData?.img && !displayPlayerScreen ?
+      {currentSongData?.img || noImage && !displayPlayerScreen ?
         <button onClick={handleDisplayPlayerScreen} className={style.image}>
           <Image
             src={currentSongData?.img || noImage}
@@ -184,19 +197,97 @@ const Player: React.FC<any> = () => {
 
 export default Player;
 
+interface PlayerScreenProps {
+  data: any;
+  volumeProgress: number
+  sendDataToParent: (data: any) => void;
+}
 
-const PlayerScreen = ({ data }: { data: any }) => {
+const PlayerScreen: React.FC<PlayerScreenProps> = ({ data, volumeProgress, sendDataToParent }) => {
+  const [progress, setProgress] = useState<number>(volumeProgress);
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    sendDataToParent(progress);
+  }, [progress])
+
+  const handleMousePosition = (e: MouseEvent | TouchEvent) => {
+    let x, y;
+    if (!svgRef.current) return;
+    const rect = svgRef.current.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+
+    if (e.type === "touchmove" || e.type === "touchstart") {
+      const touchEvent = e as TouchEvent;
+      x = touchEvent.touches[0].clientX - centerX;
+      y = touchEvent.touches[0].clientY - centerY;
+    } else if (e.type === "mousemove" || e.type === "mousedown") {
+      const mouseEvent = e as MouseEvent;
+      x = mouseEvent.clientX - centerX;
+      y = mouseEvent.clientY - centerY;
+    }
+
+    const angleRad = Math.atan2(Number(y), Number(x));
+    let angleDeg = angleRad * (180 / Math.PI);
+    angleDeg = angleDeg < 0 ? angleDeg + 360 : angleDeg;
+    if (angleDeg >= 0 && angleDeg <= 180) {
+      const progressPer = (angleDeg / 180);
+      setProgress(1 - progressPer);
+      // console.log("Progress percentage:", 1 - progressPer);
+    }
+  };
+
+  const startTracking = (e: React.MouseEvent<SVGCircleElement> | React.TouchEvent<SVGCircleElement> | any) => {
+    document.addEventListener("mousemove", handleMousePosition);
+    document.addEventListener("touchmove", handleMousePosition);
+    document.addEventListener("mouseup", stopTracking);
+    document.addEventListener("touchend", stopTracking);
+    handleMousePosition(e);
+  };
+
+  const stopTracking = () => {
+    document.removeEventListener("mousemove", handleMousePosition);
+    document.removeEventListener("touchmove", handleMousePosition);
+    document.removeEventListener("mouseup", stopTracking);
+    document.removeEventListener("touchend", stopTracking);
+  };
+
   return (
     <div className={style.songImage}>
+      <Image src={volMute} height={30} width={30} alt="mute" className={style.volMute}/>
       <div>
         <Image
           height={200}
           width={200}
           src={data?.img.replace(/(\d+)\.jpg$/, "4.jpg") || noImage}
-          // objectFit="cover"
           alt={data?.name || "no image"}
         />
       </div>
+      <svg width={340} height={340} ref={svgRef}>
+        <circle cx={170} cy={170} r={140} className={style.circle1} />
+        <circle
+          cx={170}
+          cy={170}
+          r={140}
+          className={style.circle2}
+          style={{
+            strokeDashoffset: `calc(880 - (880 * (${progress}/2)) )`
+          }}
+        />
+        <circle
+          cx={170}
+          cy={170}
+          r={140}
+          className={style.circle3}
+          onMouseDown={startTracking}
+          onTouchStart={startTracking}
+          onMouseLeave={stopTracking}
+          onTouchEnd={startTracking}
+        />
+      </svg>
+      
+          <Image src={vol} height={30} width={30} alt="high volume" className={style.vol}/>
     </div>
   );
-}
+};
