ca6dc6fecec92df43669a8006187d0235e4a7e25
BISWAJIT AICH <121601439+BiswajitAich@users.noreply.github.com>
2024-07-24 09:53:58 +0530

    Update useFetch.ts

--- app/js/useFetch.ts
+++ app/js/useFetch.ts
@@ -1,13 +1,12 @@
-const useFetch = async (endPoint: string, page?:number) => {
-        try {
-            const res = await fetch(`/api/fetchFeatured?location=${endPoint}&page=${page}`)
-            const data = await res.json()
-            // console.log(data);
-            return data
-        } catch (error) {
-            console.error("Error fetching viral data:", error);
-            return null
-        }
+const useFetch = async (endPoint: string, page: number = 1) => {
+    try {
+        const res = await fetch(`/api/fetchFeatured?location=${endPoint}&page=${page}`)
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.error("Error fetching viral data:", error);
+        return null
+    }
 }
 
 export default useFetch;
